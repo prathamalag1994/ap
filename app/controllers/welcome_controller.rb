c08c1c3dfbe41905e167d3351690b70749b9fa3b
prathamalag1994 <prathamalag@berkeley.edu>
2014-06-24 14:38:17 +0530

    alll

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-  	  #gb = Gibbon::API.new("2a6262f26c44f974bbc48801f00ca358-us8")
-	  #id = gb.campaigns.create({type: "regular", options: {list_id: "920e9dc900", subject: "Send", from_email: "prathamalag@berkeley.edu", from_name: "Red Carpets", generate_text: true}, content: {html: "<html><head></head><body><h1>Foo</h1><p>Bar</p></body></html>"}, :segment_opts => {:match => "any",  :conditions => [{field: "email", op: "eq", value: "prathamalag1994@gmail.com"}] }})
-	  #gb.campaigns.send(:cid => id["id"])
+  	  gb = Gibbon::API.new("2a6262f26c44f974bbc48801f00ca358-us8")
+	  id = gb.campaigns.create({type: "regular", options: {list_id: "920e9dc900", subject: "Send", from_email: "prathamalag@berkeley.edu", from_name: "Red Carpets", generate_text: true}, content: {html: "<html><head></head><body><h1>Foo</h1><p>Bar</p></body></html>"}, :segment_opts => {:match => "any",  :conditions => [{field: "email", op: "eq", value: "prathamalag1994@gmail.com"}] }})
+	  gb.campaigns.send(:cid => id["id"])
 
   end
 end
