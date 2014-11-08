eec9772bdb9293956d120d1bdf2615848e005a42
prathamalag1994 <prathamalag@berkeley.edu>
2014-11-07 16:25:50 -0800

    1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  @gb = Gibbon::API.new("2a6262f26c44f974bbc48801f00ca358-us8")
+  @gb = Gibbon::API.new("2a6262f26c44f974bbc48801f00ca358-us8") #now expired
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,13 +7,6 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
     build_resource(sign_up_params)
-
-    #create a linked child-class based on polymorphic associations.
-    #remember - this type parameter is coming from routes.rb and different signup types happen from different urls
-    #puts Admin_class_string.constantize
-    child_class = params[:user][:rolable_type].camelize.constantize
-    #resource.rolable = child_class.new(params[child_class.to_s.underscore.to_sym])
-    resource.rolable = child_class.new(send("#{child_class.to_s.underscore.downcase}_params"))
     
   end
 
